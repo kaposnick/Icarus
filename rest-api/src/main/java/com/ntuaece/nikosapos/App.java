60b501b4a0dc9b78f7c45799023c4fcee684215e
kaposnick <nikosapostolakis94@gmail.com>
2017-03-03 21:54:01 +0200

    Update

--- rest-api/src/main/java/com/ntuaece/nikosapos/App.java
+++ rest-api/src/main/java/com/ntuaece/nikosapos/App.java
@@ -2,7 +2,6 @@ package com.ntuaece.nikosapos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.google.gson.Gson;
 
 @SpringBootApplication
 public class App 
@@ -11,6 +10,5 @@ public class App
     public static void main( String[] args )
     { 
         SpringApplication.run(App.class, args);
-        Gson gson = new Gson();
     }
 }
