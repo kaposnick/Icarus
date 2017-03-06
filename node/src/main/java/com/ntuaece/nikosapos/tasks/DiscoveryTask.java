b8377885d80587d9c4dfd7173009f962a87607aa
kaposnick <nikosapostolakis94@gmail.com>
2017-03-06 18:27:37 +0200

    Set verbose probability for DEBUGGING Perposes

--- node/src/main/java/com/ntuaece/nikosapos/tasks/DiscoveryTask.java
+++ node/src/main/java/com/ntuaece/nikosapos/tasks/DiscoveryTask.java
@@ -3,23 +3,30 @@ package com.ntuaece.nikosapos.tasks;
 import java.util.TimerTask;
 
 import com.ntuaece.nikosapos.discover.DiscoveryService;
+import com.ntuaece.nikosapos.discover.DiscoveryServiceImpl;
 import com.ntuaece.nikosapos.entities.Node;
 
 public class DiscoveryTask extends TimerTask {
 	
 	private final Node node;
-	private final DiscoveryService discoveryService;
+	private final DiscoveryService discoveryServiceImpl;
+	private boolean verbose;
 	
 	public DiscoveryTask(Node node){
 		this.node = node;
-		this.discoveryService = new DiscoveryService(node);
+		this.discoveryServiceImpl = new DiscoveryServiceImpl(node);
+		verbose = false;
+	}
+	
+	public void setVerbose(boolean verbose){
+		this.verbose = verbose;
 	}
 
 	@Override
 	public void run() {
 		System.out.println("Node: " + node.getId() + " discovering neighbor nodes...");
-		boolean result = discoveryService.discoverForNeighbors();
-		if (result && false){
+		boolean result = discoveryServiceImpl.discoverForNeighbors();
+		if (result && verbose){
 			System.out.println("Discovery successful");
 		}
 	}
