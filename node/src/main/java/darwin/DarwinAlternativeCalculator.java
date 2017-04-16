3fda427421eb6d7a2dbc1ab5fdc2b8efe600d288
kaposnick <nikosapostolakis94@gmail.com>
2017-04-16 13:43:52 +0300

    Changed from List to Map

--- node/src/main/java/darwin/DarwinAlternativeCalculator.java
+++ node/src/main/java/darwin/DarwinAlternativeCalculator.java
@@ -1,6 +1,6 @@
 package darwin;
 
-import java.util.List;
+import java.util.Map;
 
 import com.ntuaece.nikosapos.SimulationParameters;
 import com.ntuaece.nikosapos.behaviorpacket.BehaviorUpdateEntity;
@@ -17,7 +17,7 @@ public class DarwinAlternativeCalculator implements Darwin {
     }
 
     @Override
-    public void computeDarwin(List<DarwinPacket> darwinPacketList) {
+    public void computeDarwin(Map<Long,DarwinPacket> darwinPacketList) {
         double p_I, p_minusI, p_DarwinI, p_DarwinMinusI;
         double q_I, q_minusI;
         double p_NewDarwinI, p_NewDarwinMinusI;
@@ -27,7 +27,7 @@ public class DarwinAlternativeCalculator implements Darwin {
 
         for (Neighbor neighbor : this.node.getNeighbors()) {
             neighbor.setPI(p_I);
-            
+
             p_minusI = neighbor.getPMinusI();
             p_DarwinI = neighbor.getDarwinI();
             p_DarwinMinusI = neighbor.getDarwinMinusI();
@@ -38,8 +38,8 @@ public class DarwinAlternativeCalculator implements Darwin {
             p_NewDarwinI = DarwinUtils.normalizeValue(gamma * (q_minusI - q_I));
             neighbor.setDarwinI(p_NewDarwinI);
 
-            p_NewDarwinMinusI = DarwinUtils.normalizeValue(gamma * (q_I - q_minusI));
-            neighbor.setDarwinMinusI(p_NewDarwinMinusI);
+//            p_NewDarwinMinusI = DarwinUtils.normalizeValue(gamma * (q_I - q_minusI));
+//            neighbor.setDarwinMinusI(p_NewDarwinMinusI);
 
             if (neighbor.getDarwinI() >= SimulationParameters.EDP) {
                 node.addDarwinSelfishNode(neighbor.getId());
@@ -50,24 +50,29 @@ public class DarwinAlternativeCalculator implements Darwin {
 
     }
 
-    private double calculatePI(List<DarwinPacket> darwinPacketList) {
+    private double calculatePI(Map<Long,DarwinPacket> darwinPacketList) {
         double numerator = 0;
         double denominator = 0;
-        for (DarwinPacket packet : darwinPacketList) {
+        for (DarwinPacket packet : darwinPacketList.values()) {
             Neighbor neighbor = node.findNeighborById(packet.getId()).get();
             for (BehaviorUpdateEntity entity : packet.getNeighborRatioList()) {
                 if (entity.getNeighId() == node.getId()) {
                     // cii * cji
-                    numerator += entity.getRatio() * neighbor.getConnectivityRatio();
-                    denominator += neighbor.getConnectivityRatio();
+                    // numerator += entity.getRatio() *
+                    // neighbor.getConnectivityRatio();
+                    // denominator += neighbor.getConnectivityRatio();
                     // neighbor.setPForMe(1 - entity.getRatio());
+                    numerator += entity.getΡForMe();
+                    denominator++;
+                    neighbor.setDarwinMinusI(entity.getNeighborDarwinForMe());
                 }
             }
         }
-        return (denominator > 0) ? (double) (1 - numerator / denominator) : 0;
+        return (denominator > 0) ? numerator / denominator : 0;
+//        return (denominator > 0) ? (double) (1 - numerator / denominator) : 0;
     }
 
-    private void calculatePMinusIForAllNeighbors(List<DarwinPacket> darwinPacketList) {
+    private void calculatePMinusIForAllNeighbors(Map<Long,DarwinPacket> darwinPacketList) {
         double c_minusI = 0;
         double p_minusI = 0;
 
@@ -77,7 +82,7 @@ public class DarwinAlternativeCalculator implements Darwin {
             double denominator = 0;
             double numerator = 0;
 
-            for (DarwinPacket packet : darwinPacketList) {
+            for (DarwinPacket packet : darwinPacketList.values()) {
                 if (packet.getId() != neighbor.getId()) {
                     for (BehaviorUpdateEntity entity : packet.getNeighborRatioList()) {
                         if (entity.getNeighId() == neighbor.getId()) {
