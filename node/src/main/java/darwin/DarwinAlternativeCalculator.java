2ec3a2e57e90f1a6527d74de8fd223bd6e797214
kaposnick <nikosapostolakis94@gmail.com>
2017-04-17 17:27:32 +0300

    Implemented the correct Darwin Algorithm

--- node/src/main/java/darwin/DarwinAlternativeCalculator.java
+++ node/src/main/java/darwin/DarwinAlternativeCalculator.java
@@ -17,7 +17,7 @@ public class DarwinAlternativeCalculator implements Darwin {
     }
 
     @Override
-    public void computeDarwin(Map<Long,DarwinPacket> darwinPacketList) {
+    public void computeDarwin(Map<Long, DarwinPacket> darwinPacketList) {
         double p_I, p_minusI, p_DarwinI, p_DarwinMinusI;
         double q_I, q_minusI;
         double p_NewDarwinI, p_NewDarwinMinusI;
@@ -38,8 +38,8 @@ public class DarwinAlternativeCalculator implements Darwin {
             p_NewDarwinI = DarwinUtils.normalizeValue(gamma * (q_minusI - q_I));
             neighbor.setDarwinI(p_NewDarwinI);
 
-//            p_NewDarwinMinusI = DarwinUtils.normalizeValue(gamma * (q_I - q_minusI));
-//            neighbor.setDarwinMinusI(p_NewDarwinMinusI);
+            p_NewDarwinMinusI = DarwinUtils.normalizeValue(gamma * (q_I - q_minusI));
+            neighbor.setDarwinMinusI(p_NewDarwinMinusI);
 
             if (neighbor.getDarwinI() >= SimulationParameters.EDP) {
                 node.addDarwinSelfishNode(neighbor.getId());
@@ -50,7 +50,7 @@ public class DarwinAlternativeCalculator implements Darwin {
 
     }
 
-    private double calculatePI(Map<Long,DarwinPacket> darwinPacketList) {
+    private double calculatePI(Map<Long, DarwinPacket> darwinPacketList) {
         double numerator = 0;
         double denominator = 0;
         for (DarwinPacket packet : darwinPacketList.values()) {
@@ -58,21 +58,19 @@ public class DarwinAlternativeCalculator implements Darwin {
             for (BehaviorUpdateEntity entity : packet.getNeighborRatioList()) {
                 if (entity.getNeighId() == node.getId()) {
                     // cii * cji
-                    // numerator += entity.getRatio() *
-                    // neighbor.getConnectivityRatio();
-                    // denominator += neighbor.getConnectivityRatio();
-                    // neighbor.setPForMe(1 - entity.getRatio());
-                    numerator += entity.getΡForMe();
-                    denominator++;
-                    neighbor.setDarwinMinusI(entity.getNeighborDarwinForMe());
+                    numerator += entity.getRatio() * neighbor.getConnectivityRatio();
+                    denominator += neighbor.getConnectivityRatio();
+//                     numerator += entity.getΡForMe();
+//                     denominator++;
+//                     neighbor.setDarwinMinusI(entity.getNeighborDarwinForMe());
                 }
             }
         }
-        return (denominator > 0) ? numerator / denominator : 0;
-//        return (denominator > 0) ? (double) (1 - numerator / denominator) : 0;
+//         return (denominator > 0) ? numerator / denominator : 0;
+        return (denominator > 0) ? (double) (1 - numerator / denominator) : 0;
     }
 
-    private void calculatePMinusIForAllNeighbors(Map<Long,DarwinPacket> darwinPacketList) {
+    private void calculatePMinusIForAllNeighbors(Map<Long, DarwinPacket> darwinPacketList) {
         double c_minusI = 0;
         double p_minusI = 0;
 
