3023f0a902516c29b2393ff99784c3baece94e1f
kaposnick <nikosapostolakis94@gmail.com>
2017-05-01 02:42:31 +0300

    Changed some intervals

--- node/src/main/java/node/NodeScheduledTask.java
+++ node/src/main/java/node/NodeScheduledTask.java
@@ -8,8 +8,9 @@ public final class NodeScheduledTask {
 	public final static int UPDATE_BEHAVIOR_PERIOD = 25000;
 	public final static int UPDATE_BEHAVIOR_INITIAL_DELAY = 0;
 	
-	public final static int DARWIN_UPDATE_PERIOD = 10000;
+	public final static int DARWIN_UPDATE_PERIOD = 20000;
+	public final static int ICAS_UPDATE_PERIOD = 10000;
 	
-	public final static int PACKET_SENT_PERIOD = 5000;
+	public final static int PACKET_SENT_PERIOD = 2000;
 	public final static int PACKET_SENT_INITIAL_DELAY = 500;
 }
