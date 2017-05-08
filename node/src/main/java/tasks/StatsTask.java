9ffc6119894274ff7843f7b316c940667cf7ab9f
kaposnick <nikosapostolakis94@gmail.com>
2017-05-08 12:25:48 +0300

    Distant Node File Write

--- node/src/main/java/tasks/StatsTask.java
+++ node/src/main/java/tasks/StatsTask.java
@@ -19,20 +19,26 @@ public class StatsTask implements Runnable {
 
     File coopFile;
     File selfFile;
-    BufferedWriter bf;
+    File distantFile;
 
     public StatsTask() {
         coopFile = new File("/home/nickapostol/Desktop/darwin/coop.txt");
         selfFile = new File("/home/nickapostol/Desktop/darwin/self.txt");
+        distantFile = new File("/home/nickapostol/Desktop/darwin/distant.txt");
         if (coopFile.exists()) {
             coopFile.delete();
         }
         if (selfFile.exists()) {
             selfFile.delete();
         }
+        
+        if (distantFile.exists()) {
+            distantFile.delete();
+        }
         try {
             coopFile.createNewFile();
             selfFile.createNewFile();
+            distantFile.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -49,10 +55,13 @@ public class StatsTask implements Runnable {
         try {
             FileWriter coopwriter = new FileWriter(coopFile, true);
             FileWriter selfwriter = new FileWriter(selfFile, true);
+            FileWriter distWriter = new FileWriter(distantFile,true);
             int cooperativeNodesForwarded = 0;
             int selfishNodesForwarded = 0;
             int cooperativeNodesSent = 0;
             int selfishNodesSent = 0;
+            int distantNodesSent = 0;
+            int distantNodesForwarded = 0;
             for (Node node : NodeList.GetInstance()) {
                 if (node.isCheater()) {
                     selfishNodesSent += node.getTotalPacketsSent();
@@ -61,11 +70,18 @@ public class StatsTask implements Runnable {
                     cooperativeNodesSent += node.getTotalPacketsSent();
                     cooperativeNodesForwarded += node.getTotalPacketsForwarded();
                 }
+                
+                if (node.isDistant()) {
+                    distantNodesSent += node.getTotalPacketsSent();
+                    distantNodesForwarded += node.getTotalPacketsForwarded();
+                }
             }
             coopwriter.write((float) cooperativeNodesForwarded / cooperativeNodesSent + "\n");
-            selfwriter.write((float) selfishNodesForwarded / selfishNodesSent + "\n");
             coopwriter.close();
+            selfwriter.write((float) selfishNodesForwarded / selfishNodesSent + "\n");
             selfwriter.close();
+            distWriter.write((float) distantNodesForwarded / distantNodesSent + "\n");
+            distWriter.close();
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
